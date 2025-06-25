20061c5be5869b095155d05a5b203f8d0910be94
ibrahim <ibrahim.mohamed@datagearbi.com>
2025-06-25 05:55:06 +0300

    get all employee

--- src/app/core/models/pagination.ts
+++ src/app/core/models/pagination.ts
@@ -2,5 +2,5 @@ export interface PaginationRequest {
   lang: number;
   pageNumber: number;
   pageSize: number;
-  empId?: number; 
+  empId?: number | null; // Optional, used in employee service
 }
