4480e244d778a7294c23cbc0fde2d24344795b1b
ibrahim <ibrahim.mohamed@datagearbi.com>
2025-06-25 06:40:24 +0300

    add department component

--- src/app/core/models/branch.ts
+++ src/app/core/models/branch.ts
@@ -1,4 +1,3 @@
-import { ApiResponse } from "./apiResponse";
 
 export interface Branch {
   branchId: number;
---
+++ src/app/modules/primary-data/services/department.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Department, DepartmentResponse } from '../../../core/models/department';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { PaginationRequest } from '../../../core/models/pagination';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentService {
+private apiUrl = `${environment.apiUrl}/Departments`  ;
+   
+  constructor(private http:HttpClient) { }
+  getDepartments(pagination:PaginationRequest): Observable<DepartmentResponse> {
+    const params = {
+      'lang': pagination.lang,
+      'pageNumber': pagination.pageNumber,
+      'pageSize': pagination.pageSize
+    };
+
+    return this.http.get<DepartmentResponse>(`${this.apiUrl}/GetDepartments`, { params });
+  }
+  getDepartmentById(id: number, lang: number): Observable<Department> {
+    const params = { lang: lang.toString() };
+    return this.http.get<Department>(`${this.apiUrl}/GetDepartmentById/${id}`, { params });
+  }
+  addDepartment(department: Department): Observable<Department> {
+    return this.http.post<Department>(`${this.apiUrl}/AddDepartment`, department);
+  }
+  updateDepartment(department: Department): Observable<Department> {
+    return this.http.put<Department>(`${this.apiUrl}/UpdateDepartment`, department);
+  }
+  deleteDepartment(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/DeleteDepartment/${id}`);
+  }
+}
